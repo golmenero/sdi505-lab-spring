dedbcdfd2c87b8c9b7cf5bc012c9982991850df0
golmenero <carlosgomezcolmenero@gmail.com>
2019-02-26 10:42:15 +0100

    SDI-Selenium-5.5-CasosUso-VistaRegister.

--- sdi505-lab-spring/src/test/java/com/uniovi/tests/NotaneitorTests.java
+++ sdi505-lab-spring/src/test/java/com/uniovi/tests/NotaneitorTests.java
@@ -1,6 +1,5 @@
 package com.uniovi.tests;
 
-import static org.junit.Assert.*;
 
 
 import org.junit.*;
@@ -8,9 +7,7 @@ import org.junit.runners.MethodSorters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import com.uniovi.tests.pageobjects.PO_HomeView;
-import com.uniovi.tests.pageobjects.PO_Properties;
-import com.uniovi.tests.util.SeleniumUtils;
+import com.uniovi.tests.pageobjects.*;
 
 //Ordenamos las pruebas por el nombre del método
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -62,10 +59,11 @@ public class NotaneitorTests {
 	@Test
 	public void test() {
 		/*
-		 //Cambiamos el idioma a Inglés PO_HomeView.changeIdiom(driver, "btnEnglish");
-		 //Esperamos porque aparezca que aparezca el texto de bienvenida en inglés
-		 SeleniumUtils.EsperaCargaPagina(driver, "text", p.getString("welcome.message", PO_Properties.ENGLISH), getTimeout());
-		*/ 
+		 * //Cambiamos el idioma a Inglés PO_HomeView.changeIdiom(driver, "btnEnglish");
+		 * //Esperamos porque aparezca que aparezca el texto de bienvenida en inglés
+		 * SeleniumUtils.EsperaCargaPagina(driver, "text",
+		 * p.getString("welcome.message", PO_Properties.ENGLISH), getTimeout());
+		 */
 	}
 
 	// PR01. Acceder a la página principal /
@@ -94,5 +92,35 @@ public class NotaneitorTests {
 		PO_HomeView.checkChangeIdiom(driver, "btnSpanish", "btnEnglish", PO_Properties.getSPANISH(),
 				PO_Properties.getENGLISH());
 		// SeleniumUtils.esperarSegundos(driver, 2);
+	}
+
+	// PR05. Prueba del formulario de registro. registro con datos correctos
+	@Test
+	public void PR05() {
+		// Vamos al formulario de registro
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+		// Rellenamos el formulario.
+		PO_RegisterView.fillForm(driver, "77777778A", "Josefo", "Perez", "77777", "77777");
+		// Comprobamos que entramos en la sección privada
+		PO_View.checkElement(driver, "text", "Notas del usuario");
+	}
+
+	// PR06. Prueba del formulario de registro. DNI repetido en la BD, Nombre corto,
+	// .... pagination pagination-centered, Error.signup.dni.length
+	@Test
+	public void PR06() {
+		// Vamos al formulario de registro
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+		// Rellenamos el formulario.
+		PO_RegisterView.fillForm(driver, "99999990A", "Josefo", "Perez", "77777", "77777");
+		PO_View.getP();
+		// COmprobamos el error de DNI repetido.
+		PO_RegisterView.checkKey(driver, "Error.signup.dni.duplicate", PO_Properties.getSPANISH());
+		// Rellenamos el formulario.
+		PO_RegisterView.fillForm(driver, "99999990B", "Jose", "Perez", "77777", "77777");
+		// COmprobamos el error de Nombre corto .
+		PO_RegisterView.checkKey(driver, "Error.signup.name.length", PO_Properties.getSPANISH());
+		// Rellenamos el formulario.
+		PO_RegisterView.fillForm(driver, "99999990B", "Josefo", "Per", "77777", "77777");
 	}
 }
